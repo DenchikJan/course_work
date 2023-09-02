a83fe5157a0c2461093d039d9c6c2de333a48a05
denchikjan <volchkoff.denis29@gmail.com>
2023-09-02 16:29:39 +0300

    add test_filter_list and func filter_list

--- src/func.py
+++ src/func.py
@@ -11,3 +11,13 @@ def read_file(file_name) -> list:
         raw_content = file.read()
         content = json.loads(raw_content)
     return content
+
+
+def filter_list(array: list, key_name, filter_name) -> list:
+    new_list = []
+    for elements in array:
+        if type(elements) == dict:
+            if key_name in elements.keys():
+                if elements[key_name] == filter_name:
+                    new_list.append(elements)
+    return new_list
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,6 +1,6 @@
 import pytest
 from pathlib import Path
-from src.func import read_file
+from src.func import read_file, filter_list
 
 path = Path("files", "test_file.json")
 
@@ -15,5 +15,28 @@ def array_fixture_1():
             ]
 
 
+@pytest.fixture
+def array_fixture_2():
+    return [{"first_name": "Denis", "last_name": "Volchkov"},
+            {"first_name": "Vladimir", "last_name": "Ivanov"},
+            {"first_name": "Ksenia", "last_name": "Petrova"},
+            {"first_name": "Daria", "last_name": "Mayorova"},
+            {"first_name": "Anton", "last_name": "Sidorov"},
+            6
+            ]
+
+
+@pytest.fixture
+def array_fixture_3():
+    return [{"first_name": "Denis", "last_name": "Volchkov"}]
+
+
 def test_read_file(array_fixture_1):
     assert read_file(path) == array_fixture_1
+
+
+def test_filter_dict(array_fixture_1, array_fixture_2, array_fixture_3):
+    assert filter_list(array_fixture_1, 'first_name', 'Denis') == array_fixture_3
+    assert filter_list(array_fixture_1, 'fame', 'Denis') == []
+    assert filter_list(array_fixture_2, 'first_name', 'Denis') == array_fixture_3
+    assert filter_list([], 'first_name', 'Denis') == []
